8d54d8185a6fa929c4629eedaf98aec5e4007f6e
Santiago Arbeláez <aflorezsanti@gmail.com>
2026-02-23 17:42:01 -0500

    Tercer ejercicio

---
+++ 3.CondicionalesDoblesCompuestos/3.CondicionalesDoblesCompuestos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _3.CondicionalesDoblesCompuestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string nombre = " ";
+            float sueldo = 0.0f;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Single.Parse(Console.ReadLine());
+                 //Si la condicion es verdadera
+            if (sueldo >= 3000)
+            {
+                Console.WriteLine("La persona " + nombre + " debe abonar impuestos");
+            }
+            else //Si la condición es falsa
+            { 
+                Console.WriteLine("La persona "+ nombre + " esta exenta de impuestos");
+
+            }*/
+            double num1 = 0;
+            double num2 = 0;
+            Console.WriteLine("Ingresar numero 1");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresar numero 2");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine((Convert.ToDouble(num1 + num2)) + " y " + (Convert.ToDouble((num1 - num2))));
+            }
+            else 
+            {
+                Console.WriteLine((Convert.ToDouble(num1 * num2)) + " y " + (Convert.ToDouble((num1 / num2))));
+            }
+        }
+    }
+}
