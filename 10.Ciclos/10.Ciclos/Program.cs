a8183449663489b41f72bf5f7e2778a3ea6a9525
Santiago Arbeláez <aflorezsanti@gmail.com>
2026-03-12 18:45:40 -0500

    #10 CiclosWhile ejercicios

---
+++ 10.Ciclos/10.Ciclos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace _10.Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0;
+            int conjugado = 0;
+            
+            while (num >= 0)
+            {
+                Console.WriteLine("Ingrese numeros para sumar");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                conjugado = num + conjugado;
+                Console.WriteLine(conjugado);
+            }
+            Console.WriteLine($"suma {conjugado}");
+
+
+
+        }
+    }
+}
