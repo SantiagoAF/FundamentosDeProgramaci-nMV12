ecfe68d2d614a6ced040b220b62ca42ae8ec991c
Santiago Arbeláez <aflorezsanti@gmail.com>
2026-03-17 20:45:47 -0500

    #11 y #12 EjerciciosdeCiclos

---
+++ 11.CiclosWhile/11.CiclosWhile/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _11.CiclosWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int suma = 0;
+            int impar = 1; 
+
+            while (impar <= 99)
+            {
+                suma += impar;   
+                impar += 2;      
+            }
+
+            Console.WriteLine($"La suma total es: {suma}");*/
+
+
+     
+
+            Console.WriteLine($"Ingrese la cantidad de numeros");
+            int cantidad = int.Parse(Console.ReadLine());
+            int mayoresque0 = 0;
+            int menoresque0 = 0;
+            int igualesque0 = 0;
+            int contador = 0;
+
+            while (contador < cantidad)
+            {
+                Console.Write($"Introduce el número {contador + 1}: ");
+                int numero = int.Parse(Console.ReadLine());
+                if (contador > 0)
+                {
+                    mayoresque0++;
+                }
+                else if (contador < 0)
+                {
+                    menoresque0++;
+                }
+                else
+                {
+                    igualesque0++;
+                }
+                contador++;
+            }
+       
+
+            Console.WriteLine($"La cantidad de numeros mayores que 0 es: {mayoresque0}");
+            Console.WriteLine($"La cantidad de numeros menores que 0 es: {menoresque0}");
+            Console.WriteLine($"La cantidad de numeros iguales que 0 es: {igualesque0}");
+
+        }
+    }
+}
---
+++ 12.MeCiclo/12.MeCiclo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace _12.MeCiclo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el rango de números:");
+            int rango = int.Parse(Console.ReadLine());
+
+            int contador = 1;       
+            int pares = 0;          
+
+            while (contador <= rango) 
+            {
+                if (contador % 2 == 0) 
+                {
+                    pares = pares + 1; 
+                }
+                contador = contador + 1;
+            }
+
+            Console.WriteLine("La cantidad de números pares es " + pares);
+
+        }
+    }
+}
